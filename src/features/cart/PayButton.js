a8a26d570933b5e4512d1442456364a9dc61680f
Chetna Gupta <chetnag@gmail.com>
2024-03-16 18:31:30 +0530

    changed stripe backend link

--- src/features/cart/PayButton.js
+++ src/features/cart/PayButton.js
@@ -3,32 +3,32 @@ import { useSelector } from "react-redux";
 import axios from "axios";
 
 const PayButton = ({ cartItems }) => {
-	const { currentUser } = useSelector((state) => state.auth);
-	const handleCheckout = async () => {
-		try {
-			const res = await axios.post(
-				"http://localhost:4000/create-checkout-session",
-				{
-					cartItems,
-					userId: currentUser.uid,
-				}
-			);
-			if (res.data.url) {
-				console.log(res.data.url);
-				window.location.href = res.data.url;
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	};
+  const { currentUser } = useSelector((state) => state.auth);
+  const handleCheckout = async () => {
+    try {
+      const res = await axios.post(
+        "https://stripe-backend-78dw.onrender.com/create-checkout-session",
+        {
+          cartItems,
+          userId: currentUser.uid,
+        }
+      );
+      if (res.data.url) {
+        console.log(res.data.url);
+        window.location.href = res.data.url;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	return (
-		<>
-			<button className="btn-comm" type="submit" onClick={handleCheckout}>
-				Checkout
-			</button>
-		</>
-	);
+  return (
+    <>
+      <button className="btn-comm" type="submit" onClick={handleCheckout}>
+        Checkout
+      </button>
+    </>
+  );
 };
 
 export default PayButton;

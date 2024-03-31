3b1432e21758a8be3b2b366207931ac79fb47e52
Chetna Gupta <chetnag@gmail.com>
2024-03-31 16:45:42 +0530

    added social login

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,20 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDHTZvqPA8F4bg19J8A7Wra7iOwclMhxgI",
-  authDomain: "shopping-app-d95eb.firebaseapp.com",
-  projectId: "shopping-app-d95eb",
-  storageBucket: "shopping-app-d95eb.appspot.com",
-  messagingSenderId: "954008185536",
-  appId: "1:954008185536:web:1a7a12aa54b7d02aa39b25",
-  measurementId: "G-ZZ61YZBQ4F",
+  apiKey: "AIzaSyB3muvxCd6nF2jwvIDAOxkMlTArDbS7J0s",
+  authDomain: "shopping-applive.firebaseapp.com",
+  projectId: "shopping-applive",
+  storageBucket: "shopping-applive.appspot.com",
+  messagingSenderId: "833565909494",
+  appId: "1:833565909494:web:127866dd3fbf932dbf27d7",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 

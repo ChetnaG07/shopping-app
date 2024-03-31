ed772c2b84f15a154f7b0aec167129f58225aba8
Chetna Gupta <chetnag@gmail.com>
2024-03-31 17:09:07 +0530

    added Environment Variables

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,18 +1,18 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+/* import { getAnalytics } from "firebase/analytics"; */
 
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB3muvxCd6nF2jwvIDAOxkMlTArDbS7J0s",
-  authDomain: "shopping-applive.firebaseapp.com",
-  projectId: "shopping-applive",
-  storageBucket: "shopping-applive.appspot.com",
-  messagingSenderId: "833565909494",
-  appId: "1:833565909494:web:127866dd3fbf932dbf27d7",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+/* const analytics = getAnalytics(app); */
 
 export const auth = getAuth(app);

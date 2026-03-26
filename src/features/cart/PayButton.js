49b5f32090eb79c0c192fcd8a06cf9624c0c5b84
Chetna Gupta <chetnag@gmail.com>
2026-03-26 15:45:25 +0530

    Added Loading to Payment Button

--- src/features/cart/PayButton.js
+++ src/features/cart/PayButton.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import axios from "axios";
+import { Spinner } from "react-bootstrap";
 
 const PayButton = ({ cartItems }) => {
   const { currentUser } = useSelector((state) => state.auth);
+  const [isLoading, setLoading] = useState("idle");
   const handleCheckout = async () => {
     try {
+      setLoading("loading");
       const res = await axios.post(
         "https://stripe-backend-9ch1.onrender.com/create-checkout-session",
         {
@@ -25,6 +28,16 @@ const PayButton = ({ cartItems }) => {
   return (
     <>
       <button className="btn-comm" type="submit" onClick={handleCheckout}>
+        {isLoading === "loading" ? (
+          <Spinner
+            as="span"
+            animation="border"
+            size="sm"
+            role="status"
+            aria-hidden="true"
+            style={{ display: "inline-block", marginRight: "5px" }}
+          />
+        ) : null}
         Checkout
       </button>
     </>
